bd39eeaaee5e27b7e6eacb7c9f2121c602481fc3
mr_shanmuk <shanmukmichael@gmail.com>
2021-09-20 22:32:10 +0530

    Update check_localhost.sh

--- check_localhost.sh
+++ check_localhost.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if grep "127.0.0.1" /eyc/hosts; then
+if grep "127.0.0.1" /etc/hosts; then
         echo "Everything is OK!"
 else 
         echo "E R R O R !!! 127.0.0.1 is not in /etc/hosts"
